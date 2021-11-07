3beeeb4c5fe4d2bdbb4f515f5aea496215159c74
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2021-11-07 18:55:15 +0100

    Implement options page to store user access token and submit form's URL

--- options/script.js
+++ options/script.js
@@ -1,1 +1,41 @@
-document.getElementById('myHeading').style.color = 'red'
+const getOptionsForm = () => document.getElementById('optionsForm');
+const getStorage = () => browser.storage.sync;
+
+const saveForm = event => {
+  event.preventDefault()
+
+  const optionsForm = getOptionsForm();
+  const userAccessToken = optionsForm.elements['userAccessToken'].value
+  const trackerSubmitUrl = optionsForm.elements['trackerSubmitUrl'].value
+
+  getStorage().set({userAccessToken, trackerSubmitUrl}).catch(
+    error => {
+      const message = `Error saving preferences: ${error}`
+      window.console.error(message)
+      window.alert(message)
+    }
+  )
+};
+
+const initForm = () => {
+  const optionsForm = getOptionsForm()
+  optionsForm.addEventListener('submit', saveForm)
+
+  getStorage().get(null).then(
+    results => {
+      if (results.hasOwnProperty('userAccessToken')) {
+        optionsForm.elements['userAccessToken'].value = results.userAccessToken
+      }
+      if (results.hasOwnProperty('trackerSubmitUrl')) {
+        optionsForm.elements['trackerSubmitUrl'].value = results.trackerSubmitUrl
+      }
+    },
+    error => {
+      const message = `Error loading preferences: ${error}`
+      window.console.error(message)
+      window.alert(message)
+    }
+  )
+};
+
+document.addEventListener('DOMContentLoaded', initForm)
