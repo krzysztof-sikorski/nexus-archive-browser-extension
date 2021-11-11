64730cb8fbec8b9866113d26cefdf1ab4a09e5d9
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2021-11-11 22:24:12 +0100

    Create background page to monitor Nexus Clash game traffic

--- src/background.js
+++ src/background.js
@@ -1,1 +1,21 @@
-// Put all the javascript code here, that you want to execute in background.
+/* global NexusDataQueue, NexusDataSender, Preferences, WebRequestMonitor */
+'use strict'
+
+function generateSessionId() {
+  const timestamp = Date.now()
+  const suffix = 1000000 * Math.random()
+  return `${timestamp}_${suffix}`
+}
+
+const sessionId = generateSessionId()
+
+const preferences = new Preferences()
+preferences.load()
+preferences.listenForStorageChanges()
+
+const nexusDataQueue = new NexusDataQueue()
+
+const nexusDataSender = new NexusDataSender(preferences)
+
+const webRequestMonitor = new WebRequestMonitor(sessionId, nexusDataQueue, nexusDataSender)
+webRequestMonitor.addListeners()
